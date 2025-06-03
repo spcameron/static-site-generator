a9a5234e8256896ecc829efbf7b0d7048e33aade
spcameron <cameron.seanpatrick@gmail.com>
2025-06-03 04:49:04 -0400

    Create main.py and textnode.py files

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    test_node = TextNode("This is some anchor text", "image", "https://google.com")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
