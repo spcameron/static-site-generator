b133235b7e6fafd5092a0f1bdb3c4d41aee26903
spcameron <cameron.seanpatrick@gmail.com>
2025-06-06 20:08:41 -0400

    Implement block_to_blocktype() with tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     block_strings = []
     blocks = markdown.split("\n\n")
@@ -7,3 +17,62 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         block_strings.append(block)
     return block_strings
+
+def block_to_blocktype(block):
+    if is_heading(block):
+        return BlockType.HEADING
+    elif is_code(block):
+        return BlockType.CODE
+    elif is_quote(block):
+        return BlockType.QUOTE
+    elif is_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    elif is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+def is_heading(block):
+    split_on_first_space = block.split(" ", 1)
+    leading_text = split_on_first_space[0]
+    
+    if len(leading_text) < 1 or len(leading_text) > 6:
+        return False
+    
+    for c in leading_text:
+        if c != "#":
+            return False
+        
+    return True
+
+def is_code(block):
+    if block[:3] != "```" or block[-3:] != "```":
+        return False
+    
+    return True
+
+def is_quote(block):
+    lines = block.split("\n")
+    for line in lines:
+        if line[0] != ">":
+            return False
+        
+    return True
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if line[0:2] != "- ":
+            return False
+        
+    return True
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+    i = 1
+    for line in lines:
+        if line.split(" ", 1)[0] != f"{i}.":
+            return False
+        i += 1
+        
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,9 +1,12 @@
 import unittest
 from block_markdown import (
-    markdown_to_blocks
+    BlockType,
+    markdown_to_blocks,
+    block_to_blocktype,
 )
 
 class TestBlockMarkdown(unittest.TestCase):
+    ### markdown to block tests
     def test_markdown_to_block(self):
         md = """
 This is a paragraph **written in bold** text.
@@ -22,3 +25,39 @@ This is another paragraph with _italic text_ and `inline code.`
             ],
             blocks,
         )
+        
+    ### block to blocktype tests
+    def test_paragraph_block(self):
+        block = "This is a normal paragraph block"
+        expected = BlockType.PARAGRAPH
+        self.assertEqual(block_to_blocktype(block), expected)
+
+    def test_heading_block_h1(self):
+        block = "# This is an h1 block"
+        expected = BlockType.HEADING
+        self.assertEqual(block_to_blocktype(block), expected)
+        
+    def test_heading_block_h6(self):
+        block = "###### This is an h6 block"
+        expected = BlockType.HEADING
+        self.assertEqual(block_to_blocktype(block), expected)
+        
+    def test_code_block(self):
+        block = "```This is a code block```"
+        expected = BlockType.CODE
+        self.assertEqual(block_to_blocktype(block), expected)
+        
+    def test_quote_block(self):
+        block = "> This is a quote\n> This is the second line of a quote"
+        expected = BlockType.QUOTE
+        self.assertEqual(block_to_blocktype(block), expected)
+
+    def test_unordered_list(self):
+        block = "- This is an unordered list\n- This is the second line"
+        expected = BlockType.UNORDERED_LIST
+        self.assertEqual(block_to_blocktype(block), expected)
+        
+    def test_ordered_list(self):
+        block = "1. This is an ordered list\n2. This is the second line"
+        expected = BlockType.ORDERED_LIST
+        self.assertEqual(block_to_blocktype(block), expected)
