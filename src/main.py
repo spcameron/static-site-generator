bb3cdbab1cf0713f9ec34324cadd436b4fc84c3c
spcameron <cameron.seanpatrick@gmail.com>
2025-06-07 04:23:56 -0400

    Update main.py to traverse and generate

--- src/main.py
+++ src/main.py
@@ -9,21 +9,25 @@ html_template = "template.html"
 
 def main():
     copy_static_to_public()
-    traverse_tree_and_copy(dir_path_content)
+    traverse_tree_and_generate_html(dir_path_content)
     
     
-def traverse_tree_and_copy(source_dir):
+def traverse_tree_and_generate_html(source_dir):
+    
     list_dir = os.listdir(source_dir)
+    if list_dir:
+        print(f"{source_dir} contains the following elements: {list_dir}\n")
     
     for filename in list_dir:
         source_filepath = os.path.join(source_dir, filename)
+        dest_filepath = source_filepath.replace(dir_path_content, dir_path_public)
         if os.path.isdir(source_filepath):
-            dest_filepath = source_filepath.replace(dir_path_content, dir_path_public)
-            os.mkdir(dest_filepath)
-            traverse_tree_and_copy(source_filepath)
+            os.makedirs(dest_filepath, exist_ok=True)
+            traverse_tree_and_generate_html(source_filepath)
         if os.path.isfile(source_filepath):
-            dest_filepath = source_filepath.replace(dir_path_content, dir_path_public).replace(".md", ".html")
-            generate_page(source_filepath, html_template, dest_filepath)
+            if dest_filepath[-3:] == ".md":
+                dest_filepath = dest_filepath.replace(".md", ".html")
+                generate_page(source_filepath, html_template, dest_filepath)
     
 if __name__ == "__main__":
     main()
